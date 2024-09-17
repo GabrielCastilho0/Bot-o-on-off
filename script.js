06236deaaacc42bd5a73b8a679158de3e5a16c8d
Gabriel Castilho <gabriel.ocs17@gmail.com>
2024-09-17 11:39:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// Seleciona o interruptor
 const toggleSwitch = document.getElementById('toggleSwitch');
 
-// Adiciona um evento ao mudar o estado do interruptor
 toggleSwitch.addEventListener('change', function() {
-    // Verifica se o interruptor está ligado
     if (this.checked) {
         document.body.style.backgroundColor = 'white'; // Fundo branco
     } else {
